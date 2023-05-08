2ee8e132747bf98b6fcbfc2cc768c80db4a69141
Ali <ali.moaydi2020@gmail.com>
2023-05-08 20:56:17 +0330

    3. add note objects and handlers...

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React , { useState } from 'react'
 
 // Components
 import Note from './shared/Note'
@@ -7,6 +7,43 @@ import Note from './shared/Note'
 import styles from '../styles/Landing.module.css'
 
 export default function Landing() {
+    const [data , setData] = useState([
+        {
+            title : "Hi",
+            text : "Hi my Name is Ali",
+            id: 1,
+        },
+        {
+            title : "HELLO",
+            text : "Hello my Name is M",
+            id: 2,
+        },
+        {
+            title : "Yo",
+            text : "Yoooo my Name is Ali",
+            id: 3,
+        },
+    ])
+
+    const [note , setNote] = useState("")
+    const clickHandler = (e) => {
+        const veryfiy = data.includes(note)
+        console.log(veryfiy);
+        if (note && veryfiy) {
+            saveHandler()
+        }
+        setNote(e)
+    }
+
+    const saveHandler = () => {
+        const newData = data.filter(e => e.id !== note.id);
+        newData.unshift(note)
+        setData(newData)
+    }
+    const deleteHandler = () => {
+        const newData = data.filter(e => e.id !== note.id);
+        setData(newData)
+    }
   return (
     <div className={styles.container}>
         <div className={styles.objects}>
@@ -14,12 +51,14 @@ export default function Landing() {
                 <button>+</button>
             </div>
             <div className={styles.objectsItem}>
-                <p>Salam</p>
+                {
+                    data.map(t => <p className={t.id === note.id ? styles.objSelected : ""} onClick={() => clickHandler(t)} key={t.id}>{t.title}</p>)
+                }
                 
             </div>
         </div>
         {
-            <Note />
+            note && <Note text={note} setNote={setNote} saveHandler={saveHandler} deleteHandler={deleteHandler}/>
         }
     </div>
   )
