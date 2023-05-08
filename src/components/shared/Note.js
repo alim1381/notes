afa5b4fe886908935e59aeb9f7c8c2a6c86ae6bf
Ali <ali.moaydi2020@gmail.com>
2023-05-08 20:57:21 +0330

    3. handlers

--- src/components/shared/Note.js
+++ src/components/shared/Note.js
@@ -3,15 +3,21 @@ import React from 'react'
 // Styles
 import styles from '../../styles/Note.module.css'
 
-export default function Note() {
+export default function Note({text , setNote , saveHandler , deleteHandler}) {
+    const changeHandler = (e) => {
+        setNote({
+            ...text,
+            text : e.target.value,
+        })
+    }
   return (
     <div className={styles.main}>
         <div className={styles.inputDiv} >
-            <textarea type="text" placeholder='type here ...'/>
+            <textarea onChange={changeHandler} value={text.text} type="text" placeholder='type here ...'/>
         </div>
         <div className={styles.bottomSection}>
-            <button className={styles.delete}>Delete</button>
-            <button className={styles.save}>Save</button>
+            <button className={styles.delete} onClick={() => deleteHandler()}>Delete</button>
+            <button className={styles.save} onClick={() => saveHandler()}>Save</button>
         </div>
     </div>
   )
